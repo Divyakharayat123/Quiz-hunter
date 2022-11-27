639de0057cc3616aeeed43d56546588ccaa28228
Divya Kharayat <110722818+Divyakharayat123@users.noreply.github.com>
2022-11-27 18:19:16 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        paddingBottom: 7,
+      }}
+    >
+      <p>Copyright &copy;  Divya Kharayat</p>
+    </div>
+  );
+};
+
+export default Footer;
